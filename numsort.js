663dbff4fab27ce477a397c574fe57157ec12843
joshmorrison04 <54602588+joshmorrison04@users.noreply.github.com>
2023-06-29 19:39:41 -0700

    Update numsort.js

--- numsort.js
+++ numsort.js
@@ -4,7 +4,7 @@ function userInput(){
     var num3 = prompt("Enter a third number: ")
     
     const numArray = [num1, num2, num3];
-    numArray.sort(function(a,b) {return b - a});
+    numArray.sort(function(a,b) {return a - b});
     // sort method in numArray.sort calles the compare function to compare a and b which are values entered by the user.
     // if a - b is negative, we know b is larger than a so a is therefore sorted before b
     alert("The three numbers sorted are: " + numArray);
